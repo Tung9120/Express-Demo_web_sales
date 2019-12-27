1c805d725ed6509531d13bd83ad7c9fca9c3e3aa
tung519 <vutung959595@gmail.com>
2019-12-28 02:02:54 +0700

    Add routing to index.js file

--- index.js
+++ index.js
@@ -3,8 +3,12 @@ var express = require('express');
 var app = express();
 var port = 3000;
 
-app.get('', function(req, res) {
-	res.send('Hello World!');
+app.get('/', function(req, res) {
+	res.send('Hello Express js');
+});
+
+app.get('/users', function(req, res) {
+	res.send('User list');
 });
 
 app.listen(port, function() {
