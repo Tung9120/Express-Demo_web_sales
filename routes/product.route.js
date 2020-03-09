6454fb7974050f947af4c41d10bde8e3927ac2d6
tung519 <vutung959595@gmail.com>
2020-03-10 04:06:04 +0700

    ...

---
+++ routes/product.route.js
@@ -0,0 +1,20 @@
+var express = require('express');
+
+var controller = require('../controllers/product.controller');
+var validate = require('../validate/product.validate');
+var authMiddleware = require('../middlewares/product.middleware');
+var upload = require('../middlewares/upload.middlleware');
+
+var router = express.Router();
+
+router.get('/', controller.index);
+
+router.get('/search', controller.search);
+
+router.get('/create',authMiddleware.requireAdmin, controller.create);
+
+router.get('/:id', controller.get);
+
+router.post('/create', upload.single('product_image'),authMiddleware.requireAdmin, validate.postCreate, controller.postCreate);
+
+module.exports = router;
