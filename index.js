ad64d8636ef3fb145523c1b76fe9263e7e90937a
tung519 <vutung959595@gmail.com>
2020-03-29 09:42:51 +0700

    start using mongoose npm

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ require('dotenv').config();
 var express = require('express');
 var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
+var mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGO_URL);
 
 var userRoute = require('./routes/user.route');
 var authRoute = require('./routes/auth.route');
